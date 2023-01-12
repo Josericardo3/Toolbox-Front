c1caef9ecd330390207f78822de02a871a0a9c4c
Rodrigo Gutierrez <rgutierrez1@rbi.com>
2023-01-12 08:50:37 -0500

    changed the routes and added comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import { AppMyExampleComponent } from './modules/example/example';
-
+import { AppDashboardComponent } from './modules/dashboard/app-dashboard/app-dashboard.component';
 import { AppLoginComponent } from './modules/login/app-login/app-login.component';
 import { AppRegisterComponent } from './modules/register/app-register/app-register.component';
-import { AppDashboardComponent } from './modules/dashboard/app-dashboard/app-dashboard.component';
-import { AppCaracterizacionComponent } from './modules/caracterizacion/app-caracterizacion/app-caracterizacion.component';
-AppCaracterizacionComponent
 
+
+//! First Set of routes 
 const routes: Routes = [
-  //{path: '', component: AppLoginComponent},
-  {path: 'register', component: AppRegisterComponent},
-  {path: 'dashboard', component: AppDashboardComponent},
-  {path: '', component: AppCaracterizacionComponent}
+  { path: 'login', component: AppLoginComponent },
+  { path: 'register', component: AppRegisterComponent },
+  { path: 'dashboard', component: AppDashboardComponent },
 
-  //{path: '', component: AppMyExampleComponent},
 ];
 
 @NgModule({
