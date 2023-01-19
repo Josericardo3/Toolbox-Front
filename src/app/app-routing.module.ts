395191647d07808d23b053e8302e9568ba78d317
Karen Berrio <kberrio@itech-sac.com>
2023-01-19 15:47:09 -0500

    finalizacion cambios sprint2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,57 @@
 import { NgModule } from '@angular/core';
+
 import { RouterModule, Routes } from '@angular/router';
+
+
+
 import { AppDashboardComponent } from './modules/dashboard/app-dashboard/app-dashboard.component';
+
+
+
+import { AppMyExampleComponent } from './modules/example/example';
+
+
+
 import { AppLoginComponent } from './modules/login/app-login/app-login.component';
+
 import { AppRegisterComponent } from './modules/register/app-register/app-register.component';
+
 import { AppCaracterizacionComponent } from './modules/caracterizacion/app-caracterizacion/app-caracterizacion.component';
+
 import { AppDiagnosticoComponent } from './modules/diagnostico/app-diagnostico/app-diagnostico.component';
 
-//! First Set of routes 
+
+
+
+
 const routes: Routes = [
+
   {path: '', component: AppLoginComponent},
+
+
+
   {path: 'register', component: AppRegisterComponent},
+
   {path: 'dashboard', component: AppDashboardComponent},
-  // {path: '', component: AppCaracterizacionComponent},
+
   {path: 'caracterizacion', component: AppCaracterizacionComponent},
+
   {path: 'diagnostico', component: AppDiagnosticoComponent},
+
+
+
   //{path: '', component: AppMyExampleComponent},
-  // { path: 'login', component: AppLoginComponent },
-  { path: 'register', component: AppRegisterComponent },
-  // { path: 'dashboard', component: AppDashboardComponent },
 
 ];
 
+
+
 @NgModule({
+
   imports: [RouterModule.forRoot(routes)],
+
   exports: [RouterModule]
+
 })
+
 export class AppRoutingModule { }
