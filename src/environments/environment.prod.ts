e1f5d85e238a87dca154dd2d7305e689157d1406
Josericardo3 <practicas.0003@sesitdigital.com>
2023-05-12 10:37:33 -0500

    add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: true,
 
   // apiURL:'https://www.toolbox.somee.com',
-  apiURL:'https://toolbox3-001-site1.etempurl.com',
-  apiChart: 'https://quickchart.io/chart'
+  //apiURL:'https://toolbox3-001-site1.etempurl.com',
+  apiChart: 'https://quickchart.io/chart',
 
   // apiURL:'http://10.4.3.140:8050',
- // apiURL: 'https://www.toolbox.somee.com'
+  apiURL: 'https://www.toolbox.somee.com'
 
 };
