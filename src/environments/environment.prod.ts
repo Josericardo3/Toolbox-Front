42e1a7447c966e29caee729800c6793b694df308
Josericardo3 <practicas.0003@sesitdigital.com>
2023-05-22 11:59:01 -0500

    add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
 
-  // apiURL:'https://www.toolbox.somee.com',
-  apiURL:'https://toolbox3-001-site1.etempurl.com',
+   apiURL:'https://www.toolbox.somee.com',
+  //apiURL:'https://toolbox3-001-site1.etempurl.com',
 
   // apiURL:'http://10.4.3.140:8050',
  // apiURL: 'https://www.toolbox.somee.com'
