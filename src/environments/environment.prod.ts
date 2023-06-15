8bf12e41cc0991b268c1ad6b6a697053a475d4d5
Josericardo3 <practicas.0003@sesitdigital.com>
2023-06-15 06:49:57 -0500

    fix apiURL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: true,
 
   // apiURL:'https://www.toolbox.somee.com',
-  //apiURL:'https://toolbox3-001-site1.etempurl.com',
+  apiURL:'https://toolbox31-001-site1.btempurl.com',
   apiChart: 'https://quickchart.io/chart',
 
   // apiURL:'http://10.4.3.140:8050',
   //apiURL: 'https://www.toolbox.somee.com'
-  apiURL: 'https://ec2-18-206-91-231.compute-1.amazonaws.com:8051'
+  //apiURL: 'https://ec2-18-206-91-231.compute-1.amazonaws.com:8051'
 };
